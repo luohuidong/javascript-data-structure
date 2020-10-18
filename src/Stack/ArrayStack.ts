ad46a15ca9de3266d7eeccdcb1e6ff3f353f7f5d
luohuidong <luohuidong01@126.com>
2020-10-18 23:28:52 +0800

    refactor: 更改属性名

--- src/Stack/ArrayStack.ts
+++ src/Stack/ArrayStack.ts
@@ -1,29 +1,33 @@
 import type { StackInterface } from "./types";
 
 export default class ArrayStack<T> implements StackInterface<T> {
-  private stack: T[] = [];
+  private items: T[] = [];
 
   push(element: T): void {
-    this.stack.push(element);
+    this.items.push(element);
   }
 
   pop(): T | undefined {
-    return this.stack.pop();
+    return this.items.pop();
   }
 
   peek(): T | undefined {
-    return this.stack[this.stack.length - 1];
+    return this.items[this.items.length - 1];
   }
 
   getSize(): number {
-    return this.stack.length;
+    return this.items.length;
   }
 
   isEmpty(): boolean {
-    return this.stack.length === 0;
+    return this.items.length === 0;
+  }
+
+  clear(): void {
+    this.items = [];
   }
 
   toString(): string {
-    return this.stack.join(",");
+    return this.items.join(",");
   }
 }
